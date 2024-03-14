5e30e6935f5b556f203985114adef3f2f46712b0
pavelkolechits <pavelkolechits@gmail.com>
2024-03-14 04:18:08 +0300

    add stylelint

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode, useEffect, useMemo, useState } from 'react'
-import { Theme, LOCAL_STORAGE_THEME_KEY, ThemeContext } from '../../../../shared'
+import { Theme, LOCAL_STORAGE_THEME_KEY, ThemeContext } from 'shared'
 
 interface ThemeProviderProps {
   children: ReactNode
@@ -11,7 +11,7 @@ const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) ||
 
 export const ThemeProvider = (props: ThemeProviderProps) => {
     const { children, initialTheme } = props
-    const [theme, setTheme] = useState<Theme>((initialTheme != null) || defaultTheme)
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme)
     const defaultProps = useMemo(() => ({ theme, setTheme }), [theme])
 
     useEffect(() => {
